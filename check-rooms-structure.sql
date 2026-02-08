d9afa242b29ee67f9beb75a5beccd0f5fb011cc9
Wave Deploy <deploy@example.com>
2026-02-08 13:17:10 +0100

    Update repo

--- check-rooms-structure.sql
+++
@@ -1,5 +0,0 @@
--- Run this first to see what columns your rooms table currently has
-SELECT column_name, data_type, is_nullable
-FROM information_schema.columns
-WHERE table_name = 'rooms'
-ORDER BY ordinal_position;
